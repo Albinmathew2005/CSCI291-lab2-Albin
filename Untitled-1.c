7e921a082d780ead1a415a3e6afb0f40e1b80ec5
Albinmathew2005 <amv999@uowmail.edu.au>
2024-09-25 18:19:30 +0400

    Add files via upload

---
+++ Untitled-1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#define espresso_coffee_grams 8
+#define cappucino_coffee_grams 8 
+#define mocha_coffee_grams 8 
+#define espresso_water 30
+#define cappcucino_water 30 
+#define mocha_water 39
+#define espresso_milk 0 
+#define cappucino_milk 70 
+#define mocha_milk 160
+#define espresso_chocolate 0
+#define mocha_chocolate 30 
+#define cappucino_chocolate 0
+#define espresso_price 3.5
+#define cappucino_price 4.5
+#define mocha_price 5.5
+
+
+
+
+
+
+
+
+
+int main ()
+{
+
+
+
+
+
+
+}
